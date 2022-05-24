161beec80d5bd58cd8fbe462298954a0a81f48a7
Skorp <alexbarack13@gmail.com>
2022-05-24 06:15:30 +0300

    Add project files.

---
+++ LostAndFoundPatras.Android/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using Org.Json;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Auth.Api;
+using Android.Content;
+using Android.Gms.Common.Apis;
+using LostAndFoundPatras.Droid;
+
+namespace LostAndFoundPatras.Droid
+{
+    [Activity(Label = "LostAndFoundPatras", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1)
+            {
+                GoogleSignInResult result = Auth.GoogleSignInApi.GetSignInResultFromIntent(data);
+                GoogleManager.Instance.OnAuthCompleted(result);
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ LostAndFoundPatras/HeaderContentView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LostAndFoundPatras
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeaderContentView : ContentView
+    {
+        private readonly IGoogleManager _googleManager;
+        GoogleUser GoogleUser = new GoogleUser();
+        public bool IsLogedIn { get; set; }
+        public HeaderContentView()
+        {
+            _googleManager = DependencyService.Get<IGoogleManager>();
+           // CheckUserLoggedIn();
+            InitializeComponent();
+        }
+        //private void CheckUserLoggedIn()
+        //{
+        //    _googleManager.Login(OnLoginComplete);
+        //}
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            
+            if (btnLogin.Text == "Logout")
+            {
+              GoogleLogout();
+            }
+            else
+                _googleManager.Login(OnLoginComplete);
+        }
+        void GoogleLogout()
+        {
+            _googleManager.Logout();
+            IsLogedIn = false;
+            btnLogin.Text = "Login";
+            Username.Text = "";
+            Email.Text = "";
+            imgProfile.Source = "userimage.png";
+        }
+        private void OnLoginComplete(GoogleUser googleUser, string message)
+        {
+            if (googleUser != null)
+            {
+                GoogleUser = googleUser;
+                Username.Text = GoogleUser.Name;
+                Email.Text = GoogleUser.Email;
+                imgProfile.Source = GoogleUser.Picture;
+                IsLogedIn = true;
+                btnLogin.Text = "Logout";
+            }
+        }
+    }
+}
